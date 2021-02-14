eea08c4074113375563af7baeaecb83f8a0fe383
Peter Somogyvari <peter.somogyvari@accenture.com>
2021-02-13 23:47:19 -0800

    fix: specify cli file with absolute path
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ echo "$(env)"
 echo "::lint-git-repo-request::Sending request to DCI Lint API..."
 
 RESPONSE=$(node \
-  ./pkg/cmd-api-server/dist/lib/main/typescript/cmd/dci-lint-cli.js \
+  /usr/src/app/pkg/cmd-api-server/dist/lib/main/typescript/cmd/dci-lint-cli.js \
   lint-git-repo \
   --request=$1)
 
