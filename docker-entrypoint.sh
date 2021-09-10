87910e53081cc31aa4ec3e35d190889d92a7cd6f
Peter Somogyvari <peter.somogyvari@accenture.com>
2021-09-09 19:24:50 -0700

    fix: single quotes around JSON request parameter
    Potential fix, not a sure thing.
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 
 echo "$(env)"
 echo "=========="
-echo "$1"
+echo "'$1'"
 echo "::lint-git-repo-request::Sending request to DCI Lint API..."
 
 RESPONSE=$(node \
   /usr/src/app/node_modules/@dci-lint/cmd-api-server/dist/lib/main/typescript/cmd/dci-lint-cli.js \
   lint-git-repo \
-  --request=$1)
+  --request='$1')
 
 echo "::set-output name=lint-git-repo-response::$RESPONSE"
