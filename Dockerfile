2298e13d8708946ee5d82ff732f144851388e993
Peter Somogyvari <peter.somogyvari@accenture.com>
2021-09-09 23:02:05 -0700

    fix: status check in shell script
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM ghcr.io/petermetz/dci-lint:0.4.0
 
+USER root
+RUN apt update && apt install -y jq
+USER ${APP_USER}
+
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,18 @@ echo "::lint-git-repo-request::Sending request to DCI Lint API..."
 
 RESPONSE=`node /usr/src/app/node_modules/@dci-lint/cmd-api-server/dist/lib/main/typescript/cmd/dci-lint-cli.js lint-git-repo --request="$1"`
 
+
+echo "DCI_LINT_OUTPUT<<EOF" >> $GITHUB_ENV
+echo "$DCI_LINT_OUTPUT" >> $GITHUB_ENV
+echo "EOF" >> $GITHUB_ENV
+
 echo "::set-output name=lint-git-repo-response::$RESPONSE"
+
+LINTER_ERROR_COUNT=`echo $RESPONSE | jq -r '.linterErrors | length'`
+if [ "$LINTER_ERROR_COUNT" -gt "0" ]; then
+  echo "Found $LINTER_ERROR_COUNT linter errors. Failing the check..."
+  exit 1
+else
+  echo "No linter errors found. Passing the check..."
+  exit 0
+fi
