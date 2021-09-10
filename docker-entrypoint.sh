76a38eaf37a1dcf4546a90a0da83f0f8d1d72878
Peter Somogyvari <peter.somogyvari@accenture.com>
2021-09-09 23:13:23 -0700

    feat: new output variable, fix output formatting of JSON
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,15 +5,11 @@ echo "$1"
 echo "::lint-git-repo-request::Sending request to DCI Lint API..."
 
 RESPONSE=`node /usr/src/app/node_modules/@dci-lint/cmd-api-server/dist/lib/main/typescript/cmd/dci-lint-cli.js lint-git-repo --request="$1"`
-
-
-echo "DCI_LINT_OUTPUT<<EOF" >> $GITHUB_ENV
-echo "$DCI_LINT_OUTPUT" >> $GITHUB_ENV
-echo "EOF" >> $GITHUB_ENV
-
-echo "::set-output name=lint-git-repo-response::$RESPONSE"
-
 LINTER_ERROR_COUNT=`echo $RESPONSE | jq -r '.linterErrors | length'`
+
+echo "::set-output name=lint-git-repo-response::${RESPONSE//[$'\t\r\n ']}"
+echo "::set-output name=lint-git-repo-error-count::$LINTER_ERROR_COUNT"
+
 if [ "$LINTER_ERROR_COUNT" -gt "0" ]; then
   echo "Found $LINTER_ERROR_COUNT linter errors. Failing the check..."
   exit 1
