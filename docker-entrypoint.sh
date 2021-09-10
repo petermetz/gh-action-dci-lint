5221b9beed5572758135dcf641ae95172a504761
Peter Somogyvari <peter.somogyvari@accenture.com>
2021-09-09 22:14:19 -0700

    fix(docker-entrypoint): output is no longer swalloed by the -e flag
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-set -e
-
-echo "$(env)"
-echo "=========="
+echo "Request JSON:"
 echo "$1"
 echo "::lint-git-repo-request::Sending request to DCI Lint API..."
 
-RESPONSE=$(node \
-  /usr/src/app/node_modules/@dci-lint/cmd-api-server/dist/lib/main/typescript/cmd/dci-lint-cli.js \
-  lint-git-repo \
-  --request="$1")
+RESPONSE=`node /usr/src/app/node_modules/@dci-lint/cmd-api-server/dist/lib/main/typescript/cmd/dci-lint-cli.js lint-git-repo --request="$1"`
 
 echo "::set-output name=lint-git-repo-response::$RESPONSE"
