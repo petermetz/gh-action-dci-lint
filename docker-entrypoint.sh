7b6417c95b8df49f280d8b77833ff6c82c19bd03
Peter Somogyvari <peter.somogyvari@accenture.com>
2021-09-15 12:48:20 -0700

    fix: entrypoint now examines exit code not just stdout
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,12 +8,17 @@ RESPONSE=`node /usr/src/app/node_modules/@dci-lint/cmd-api-server/dist/lib/main/
 echo "DCI-Lint JSON Response:"
 echo "$RESPONSE"
 
+LINTER_EXIT_CODE=$?
+
 LINTER_ERROR_COUNT=`echo $RESPONSE | jq -r '.linterErrors | length'`
 
 echo "::set-output name=lint-git-repo-response::${RESPONSE//[$'\t\r\n ']}"
 echo "::set-output name=lint-git-repo-error-count::$LINTER_ERROR_COUNT"
 
-if [ "$LINTER_ERROR_COUNT" -gt "0" ]; then
+if [ "$LINTER_EXIT_CODE" -gt "0" ]; then
+  echo "DCI-Lint exited with code $LINTER_EXIT_CODE. Failing the check..."
+  exit 2
+elif [ "$LINTER_ERROR_COUNT" -gt "0" ]; then
   echo "Found $LINTER_ERROR_COUNT linter errors. Failing the check..."
   exit 1
 else
